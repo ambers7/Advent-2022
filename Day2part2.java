121c781fcff89c97bab2f5d3b877917610bca5d5
Amber Shen <ambrshen@gmail.com>
2022-12-02 09:21:42 -0500

    Start day 2

--- Day2part2.java
+++ Day2part2.java
@@ -8,6 +8,49 @@ public class Day2part2 {
     try {
       File file = new File("Day2test.txt");
       Scanner input = new Scanner(file);
+      ArrayList<String> opp = new ArrayList<String>();
+      ArrayList<String> winlosedraw = new ArrayList<String>();
+      while (input.hasNext()) {
+        opp.add(input.next());
+        winlosedraw.add(input.next());
+      }
+      System.out.println(opp);
+      System.out.println(winlosedraw);
+      int score = 0;
+      for (int i = 0;i<opp.size();i++) {
+        //add score for shape you selected
+        if (you.get(i).equals("X")) { //rock
+          score+=1;
+        } else if (you.get(i).equals("Y")) { //paper
+          score+=2;
+        } else { //scissors
+          score+=3;
+        }
+        //win lose points
+        //A,X: rock, B,Y: paper, C,Z: scissors
+        if (opp.get(i).equals("A")) { //rock
+          if (you.get(i).equals("Y")) { //paper
+            score+=6;
+          } else if (you.get(i).equals("X")) {//draw
+            score+=3;
+          }
+        }
+        if (opp.get(i).equals("B")) { //paper
+          if (you.get(i).equals("Z")) { //scissors
+            score+=6;
+          } else if (you.get(i).equals("Y")) {//draw
+            score+=3;
+          }
+        }
+        if (opp.get(i).equals("C")) { //scissors
+          if (you.get(i).equals("X")) { //rock
+            score+=6;
+          } else if (you.get(i).equals("Z")) {//draw
+            score+=3;
+          }
+        }
+      }
+      System.out.println(score);
       input.close();
     } catch (FileNotFoundException ex) {
       System.out.println("File not found");
