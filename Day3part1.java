c890dffa325993bf8970c28456c2fd484745f3f1
ambers7 <ambrshen@gmail.com>
2022-12-03 11:44:15 -0500

    Day 3 part 1

--- Day3part1.java
+++ Day3part1.java
@@ -8,20 +8,29 @@ public class Day3part1 {
     try {
       File file = new File("Day3test.txt");
       Scanner input = new Scanner(file);
-      System.out.println(mostCommon("vJrwpWtwJgWr","hcsFMMfFFhFp"));
-      System.out.println(mostCommon("jqHRNqRjqzjGDLGL","rsFMfFZSrLrFZsSL"));
-      System.out.println(mostCommon("PmmdzqPrV","vPwwTWBwg"));
+      int prioritysum = 0;
       while (input.hasNext()) {
         String line = input.nextLine();
+        int splitindex = line.length()/2;
+        String firsthalf = line.substring(0,splitindex);
+        String secondhalf = line.substring(splitindex);
+        char mostCommon = mostCommon(firsthalf,secondhalf);
+        char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+        int priority = 0;
+        for (int i = 0;i<alphabet.length;i++) {
+          if (mostCommon==alphabet[i]) {
+            priority = i+1;
+          }
+        }
+        prioritysum+=priority;
       }
+      System.out.println(prioritysum);
       input.close();
     } catch (FileNotFoundException ex) {
       System.out.println("file not found");
     }
   }
   private static char mostCommon(String str1, String str2) {
-    int[] occurence = new int[52];
-    char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     char mostCommon = 'a';
     for (int i = 0;i<str1.length();i++) {
       for (int j = 0;j<str2.length();j++) {
