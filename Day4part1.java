4729c787e07ea7f466eff15daecdb45b7a9a6a2d
ambers7 <ambrshen@gmail.com>
2022-12-04 00:51:00 -0500

    day 4 part 1

--- Day4part1.java
+++ Day4part1.java
@@ -8,6 +8,28 @@ public class Day4part1 {
     try {
       File file = new File("Day4test.txt");
       Scanner input = new Scanner(file);
+      int rangepair = 0;
+      while (input.hasNext()) {
+        String line = input.nextLine();
+        String[] onetwo = line.split(",");
+        String one = onetwo[0];
+        String two = onetwo[1];
+        String[] onesplit = one.split("-");
+        String[] twosplit = two.split("-");
+        int onestart = Integer.parseInt(onesplit[0]);
+        int oneend = Integer.parseInt(onesplit[1]);
+        int twostart = Integer.parseInt(twosplit[0]);
+        int twoend = Integer.parseInt(twosplit[1]);
+        //one holds two
+        if (onestart<=twostart&&oneend>=twoend) {
+          rangepair+=1;
+          System.out.println(line+", onestart: "+onestart+", oneend: "+oneend+", twostart: "+twostart+", twoend: "+twoend);
+        } else if (twostart<=onestart&&twoend>=oneend) { //two holds one
+          rangepair+=1;
+          System.out.println(line+", onestart: "+onestart+", oneend: "+oneend+", twostart: "+twostart+", twoend: "+twoend);
+        }
+      }
+      System.out.println(rangepair);
     } catch (FileNotFoundException ex) {
       System.out.println("file not found");
     }
